4d46374af724989f226b44ad2ede1a2d2bd79e56
harshali4827 <harshali4827@gmail.com>
2025-09-30 15:53:12 +0530

    initial commit

--- src/config.js
+++ src/config.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 const config = {
   //  baseURL: import.meta.env.VITE_API_BASE_URL
-   baseURL: 'http://192.168.1.5:5000/api'
+  //  baseURL: 'http://192.168.1.5:5000/api'
+
+  baseURL: 'http://localhost:5000/api'
 
 };
 
