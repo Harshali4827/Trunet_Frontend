e800ccbe49e4030f3684345237342e644bdd4c3c
harshali4827 <harshali4827@gmail.com>
2025-09-30 16:06:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import React, { Suspense, useEffect } from 'react'
+import React, { Suspense } from 'react'
 import { HashRouter, Route, Routes } from 'react-router-dom'
-import { useSelector } from 'react-redux'
 
-import { CSpinner, useColorModes } from '@coreui/react-pro'
+import { CSpinner } from '@coreui/react-pro'
 import './scss/style.scss'
 
 import './scss/examples.scss'
@@ -20,25 +19,6 @@ const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 const EmailApp = React.lazy(() => import('./views/apps/email/EmailApp'))
 
 const App = () => {
-  const { isColorModeSet, setColorMode } = useColorModes(
-    'coreui-pro-react-admin-template-theme-bright',
-  )
-  const storedTheme = useSelector((state) => state.theme)
-
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.href.split('?')[1])
-    const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0]
-    if (theme) {
-      setColorMode(theme)
-    }
-
-    if (isColorModeSet()) {
-      return
-    }
-
-    setColorMode(storedTheme)
-  }, [])
-
   return (
     <HashRouter>
       <Suspense
