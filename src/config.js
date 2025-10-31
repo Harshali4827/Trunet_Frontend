7414007f3389143afee3a54f80b987e10b0d3afb
harshali4827 <harshali4827@gmail.com>
2025-10-31 14:55:48 +0530

    to fetch all return data

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ axios.defaults.withCredentials = true;
 const config = {
   //  baseURL: import.meta.env.VITE_API_BASE_URL
 
-     // baseURL: 'http://localhost:5000/api'
+     //baseURL: 'http://localhost:5000/api'
      baseURL: 'http://103.175.190.244/api'
 
 };
